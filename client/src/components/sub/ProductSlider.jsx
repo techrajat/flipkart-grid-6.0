aa919a34317bfec965fd949d2dd45a3171b08af1
techrajat <rajatkumar100095@gmail.com>
2024-08-30 00:33:00 +0530

    Product search through avatar developed.

--- client/src/components/sub/ProductSlider.jsx
+++ client/src/components/sub/ProductSlider.jsx
@@ -4,14 +4,13 @@ import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules'; 
-import ProductCard from './ProductCard'; // Import your ProductCard component
-import background from '../../assets/banner-bg.png';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import ProductCard from './ProductCard';
 
 const ProductSlider = ({ products }) => {
   return (
     <div className="relative  px-4 py-12 shadow-lg shadow-blue-200 rounded-md z-[50]">
-    
+
       <Swiper
         effect={'coverflow'}
         grabCursor={true}
@@ -21,8 +20,8 @@ const ProductSlider = ({ products }) => {
           delay: 5000,
           disableOnInteraction: true,
         }}
-        slidesPerView={3}  
-        spaceBetween={30}  
+        slidesPerView={3}
+        spaceBetween={30}
         coverflowEffect={{
           rotate: 50,
           stretch: -100,
@@ -44,7 +43,7 @@ const ProductSlider = ({ products }) => {
             <ProductCard product={product} />
           </SwiperSlide>
         ))}
-        
+
         <div className="swiper-controls">
           <div className="swiper-button-prev slider-arrow">
             <ion-icon name="arrow-back-outline"></ion-icon>
